8778dc57da314541283ee89d5c306896f995146d
lackingworth <kotanwot@gmail.com>
2025-07-08 10:57:40 +0300

    Default server setup

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import ()
+import (
+	"log"
+	"net/http"
+)
+
+const (
+	PORT = ":8080"
+)
 
 func main() {
 
+	mux := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":" + PORT,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port: %s\n", PORT)
+	log.Fatal(server.ListenAndServe())
 }
