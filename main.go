cc5d17220f94d816c4125ec39225669865900720
lackingworth <kotanwot@gmail.com>
2025-07-11 13:52:58 +0300

    Added simple GET functionality

--- main.go
+++ main.go
@@ -75,6 +75,8 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
 	mux.HandleFunc("POST /api/chirps", apiCfg.handlerCreateChirp)
+	mux.HandleFunc("GET /api/chirps", apiCfg.handlerGetChirps)
+	mux.HandleFunc("GET /api/chirps/{chirpID}", apiCfg.handlerGetChirpsByID)
 
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -6,3 +6,9 @@ INSERT INTO chirps (id, created_at, updated_at, body, user_id) VALUES (
                                                                  $1,
                                                                  $2
                                                              ) RETURNING *;
+
+-- name: GetChirps :many
+SELECT * FROM chirps ORDER BY created_at;
+
+-- name: GetChirp :one
+SELECT * FROM chirps WHERE id = $1;
