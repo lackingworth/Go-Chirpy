a43264119bf650cb643e0a83d33d8df5a4c53c7b
lackingworth <kotanwot@gmail.com>
2025-07-14 18:40:12 +0300

    Added sorting queries

--- handler_get_chirps.go
+++ handler_get_chirps.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"sort"
 	"time"
 
 	"github.com/google/uuid"
@@ -17,14 +18,33 @@ type Chirp struct {
 
 func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 
-	chirps, dbErr := cfg.db.GetChirps(r.Context())
-	if dbErr != nil {
-		respondWithError(w, http.StatusInternalServerError, "Can't get chirps", dbErr)
+	chirps, err := cfg.db.GetChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Can't get chirps", err)
 		return
+	}
+
+	authorID := uuid.Nil
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err = uuid.Parse(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID", err)
+			return
+		}
+	}
+
+	sortDirection := "asc"
+	sortDirectionParam := r.URL.Query().Get("sort")
+	if sortDirectionParam == "desc" {
+		sortDirection = "desc"
 	}
 
 	returnedSlice := make([]Chirp, 0, len(chirps))
 	for _, chirp := range chirps {
+		if authorID != uuid.Nil && chirp.UserID != authorID {
+			continue
+		}
 		returnedInstance := Chirp{
 			ID:        chirp.ID,
 			CreatedAt: chirp.CreatedAt,
@@ -34,6 +54,14 @@ func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, r *http.Request) {
 		}
 		returnedSlice = append(returnedSlice, returnedInstance)
 	}
+
+	sort.Slice(returnedSlice, func(i, j int) bool {
+		if sortDirection == "desc" {
+			return returnedSlice[i].CreatedAt.After(returnedSlice[j].CreatedAt)
+		}
+		return returnedSlice[i].CreatedAt.Before(returnedSlice[j].CreatedAt)
+	})
+
 	respondWithJSON(w, http.StatusOK, returnedSlice)
 }
 
